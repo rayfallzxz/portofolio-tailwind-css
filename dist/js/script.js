a58373e2d7d816b06714a54755dd74459c5325fa
RayhanN <rayhantkjx@gmail.com>
2023-02-16 12:29:51 +0700

    update dark mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function() {
     const header = document.querySelector('header');
     const fixnav = header.offsetTop;
+    const toTop = document.querySelector('#totop');
 
     if(window.pageYOffset > fixnav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed')
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
 }
 
@@ -18,3 +23,32 @@ hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active');
     navmenu.classList.toggle('hidden')
 });
+
+//Klik Luar Hamburger
+window.addEventListener('click', function (e) {
+    if(e.target != hamburger && e.target != navmenu) {
+        hamburger.classList.remove('hamburger-active');
+        navmenu.classList.add('hidden')
+    }
+});
+
+//Dark mode
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function() {
+    if (darkToggle.checked) {
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    } else {
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
+
+//Pindah Toggle sesuai Mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+} else {
+    darkToggle.checked = false;
+}
